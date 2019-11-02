74188e8866b2986d8addb88e42af24e0c84bd67d
Zak Forster <zak.forster16@gmail.com>
2019-11-02 22:24:59 +0000

    :'(

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import operator
-
+import math
 # we want to find the steady state probability
 # Run the model from task 2 for 3 time steps. Based on 10000 repetitions report the probability and
 # standard deviation that at time step three (that is after three updates) the turtle is at cell 1,3,9.
@@ -48,10 +48,27 @@ def tower_sample(current_state: str, chain: dict):
             # print("stopping sum as r {} which is smaller than the sum: {}".format(r, sum_of_t_elements))
             # print("the state that r is smaller than or equal to is: {}".format(t[probability]))
             state = t[probability][0]
-            print('go to state {}'.format(state))
+            # print('go to state {}'.format(state))
             break
     return int(state)
 
+
+def get_mean(listof: list, runs: int):
+    i = 0
+    for num in listof:
+        i = i + num
+    return i / runs
+
+
+def get_standard(listof: list, mean: float):
+    thing1 = []
+    for i in listof:
+        thing1.append((i - mean) ** 2)
+    sumof = 0
+    for i in thing1:
+        sumof = sumof + i
+    variance = ((1/len(thing1) * sumof))
+    return (math.sqrt(variance))
 
 if __name__ == '__main__':
     # below we have a markov chain that contains the transition probabilities, so now we do not need the metropolis algorithm
@@ -71,22 +88,48 @@ if __name__ == '__main__':
     times_in_each_state_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     current_state = random.choice(states)
-    print("START: {}".format(current_state))
+    print("STARTING STATE: {}".format(current_state))
     time_steps = 3
     repetitions = 10000
+    total_runs = 30
 
-    chosen_state = None
-    for time_step in range(1, time_steps + 1):
+    one = []
+    nine = []
+    three = []
+
+    for i in range(0, total_runs):
         for repetition in range(1, repetitions + 1):
-            chosen_state = tower_sample(current_state=current_state, chain=chain)
-            # print("transitioning to state: {}".format(chosen_state))
-            current_state = str(chosen_state)
-        times_in_each_state_count[chosen_state] = times_in_each_state_count[chosen_state] + 1
+            for time_step in range(1, time_steps + 1):
+                chosen_state = tower_sample(current_state=current_state, chain=chain)
+                current_state = str(chosen_state)
+            times_in_each_state_count[chosen_state] = times_in_each_state_count[chosen_state] + 1
+        print(times_in_each_state_count)
+        probability_of_being_in_1 = times_in_each_state_count[1] / repetitions
+        probability_of_being_in_2 = times_in_each_state_count[2] / repetitions
+        probability_of_being_in_3 = times_in_each_state_count[3] / repetitions
+        probability_of_being_in_4 = times_in_each_state_count[4] / repetitions
+        probability_of_being_in_5 = times_in_each_state_count[5] / repetitions
+        probability_of_being_in_6 = times_in_each_state_count[6] / repetitions
+        probability_of_being_in_7 = times_in_each_state_count[7] / repetitions
+        probability_of_being_in_8 = times_in_each_state_count[8] / repetitions
+        probability_of_being_in_9 = times_in_each_state_count[9] / repetitions
+        print('Estimated probability of being in state 1: {}'.format(probability_of_being_in_1))
+        print('Estimated probability of being in state 3: {}'.format(probability_of_being_in_3))
+        print('Estimated probability of being in state 9: {}'.format(probability_of_being_in_9))
+        one.append(probability_of_being_in_1)
+        three.append(probability_of_being_in_3)
+        nine.append(probability_of_being_in_9)
+        times_in_each_state_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     print(" ")
-    print(times_in_each_state_count)
-    probability_of_being_in_1 = times_in_each_state_count[1] / time_steps
-    probability_of_being_in_3 = times_in_each_state_count[3] / time_steps
-    probability_of_being_in_9 = times_in_each_state_count[9] / time_steps
-    print(probability_of_being_in_1)
-    print(probability_of_being_in_3)
-    print(probability_of_being_in_9)
+
+    mean_one = (get_mean(listof=one, runs=total_runs))
+    standard_deviation_one = get_standard(listof=one, mean=mean_one)
+    print("probability of state one: {}, standard deviation: {}".format(mean_one, standard_deviation_one))
+
+    mean_three = (get_mean(listof=three, runs=total_runs))
+    standard_deviation_three = get_standard(listof=three, mean=mean_three)
+    print("probability of state three: {}, standard deviation: {}".format(mean_three, standard_deviation_three))
+
+    mean_nine = (get_mean(listof=nine, runs=total_runs))
+    standard_deviation_nine = get_standard(listof=nine, mean=mean_nine)
+    print("probability of state nine: {}, standard deviation: {}".format(mean_nine, standard_deviation_nine))
